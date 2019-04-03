2e6233ce7e29bf8da16004c5f7946aae58271acb
João Pedro Vieira <jp-a.v@hotmail.com>
2019-04-03 13:36:32 -0300

    Atualizado método getPessoas

--- AS01/2.Sistema/pjns/src/main/java/com/github/moraesofia/pjns/files/ArquivoJns.java
+++ AS01/2.Sistema/pjns/src/main/java/com/github/moraesofia/pjns/files/ArquivoJns.java
@@ -5,6 +5,19 @@ import com.github.moraesofia.pjns.entities.Pessoa;
 import com.github.moraesofia.pjns.entities.Premiacao;
 import com.github.moraesofia.pjns.entities.Premio;
 
+import com.github.moraesofia.pjns.database.DatabaseConnection;
+import com.github.moraesofia.pjns.entities.enums.CargoEnum;
+import com.github.moraesofia.pjns.entities.enums.GeneroEnum;
+
+import java.sql.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.io.IOException;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,7 +32,31 @@ public class ArquivoJns {
 
     private List<Premio> premios = new ArrayList<>();
 
-    public List<Pessoa> getPessoas() {
+    public List<Pessoa> getPessoas() throws ClassNotFoundException, SQLException,
+            InstantiationException, IllegalAccessException, IOException {
+        final Connection connection = DatabaseConnection.connect();
+        try {
+            final PreparedStatement s = connection.prepareStatement("SELECT id,nome,cargo,nascimento,genero " +
+                    "FROM pessoa");
+            final ResultSet r = s.executeQuery();
+            while (r.next()) {
+                final Integer id = (Integer) r.getObject("id");
+                final String nome = (String) r.getObject("nome");
+                final String cargo = (String) r.getObject("cargo");
+                final Integer nascimento = (Integer) r.getObject("nascimento");
+                final String genero = (String) r.getObject("genero");
+
+                final Pessoa pessoa = new Pessoa();
+                pessoa.setId(id);
+                pessoa.setNome(nome);
+                pessoa.setCargo(CargoEnum.valueOf(cargo));
+                pessoa.setNascimento(nascimento);
+                pessoa.setGenero(GeneroEnum.valueOf(genero));
+                pessoas.add(pessoa);
+            }
+        } catch (SQLException sqlE) {
+            sqlE.printStackTrace();
+        }
         return pessoas;
     }
 
