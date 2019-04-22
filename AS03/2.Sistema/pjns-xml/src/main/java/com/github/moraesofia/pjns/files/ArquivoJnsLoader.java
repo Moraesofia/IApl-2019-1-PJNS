dacde89943c48985064b734a4b2fc191534adac0
João Pedro Vieira <jp-a.v@hotmail.com>
2019-04-22 19:50:30 -0300

    Atualizada classe ArquivoJnsLoader

--- AS03/2.Sistema/pjns-xml/src/main/java/com/github/moraesofia/pjns/files/ArquivoJnsLoader.java
+++ AS03/2.Sistema/pjns-xml/src/main/java/com/github/moraesofia/pjns/files/ArquivoJnsLoader.java
@@ -1,5 +1,8 @@
 package com.github.moraesofia.pjns.files;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -12,8 +15,14 @@ public class ArquivoJnsLoader {
      * @throws FileNotFoundException Caso o arquivo não seja encontrado.
      */
     public ArquivoJns load(File file) throws FileNotFoundException {
-        // Seção 4 desse site tem como faz:
-        // https://www.mkyong.com/java/jaxb-hello-world-example/
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(ArquivoJns.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            ArquivoJns arquivoJns = (ArquivoJns) jaxbUnmarshaller.unmarshal(file);
+            return arquivoJns;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
