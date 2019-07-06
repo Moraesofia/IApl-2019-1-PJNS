e9fcdb61af6a47009292d0cb2358966a708b8b25
NataliaLopes <natalialopescig@gmail.com>
2019-07-05 23:33:02 -0300

    Countrys Adicionado

--- AS04/2.Sistema/src/main/java/com/crossover/jns/JnsFilmes/presentation/website/producer/CountryController.java
+++ AS04/2.Sistema/src/main/java/com/crossover/jns/JnsFilmes/presentation/website/producer/CountryController.java
@@ -1,10 +1,36 @@
 package com.crossover.jns.JnsFilmes.presentation.website.producer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.crossover.jns.JnsFilmes.business.service.producer.CountryService;
+import com.crossover.jns.JnsFilmes.exceptions.NotFoundException;
+import com.crossover.jns.JnsFilmes.exceptions.ProducerApiException;
+import com.crossover.jns.JnsFilmes.exceptions.WebsiteException;
+import com.crossover.jns.JnsFilmes.presentation.dto.producer.CountryDto;
 
 @Controller
 @RequestMapping("/countries")
 public class CountryController {
 
+	@Autowired
+	private CountryService countryService;
+
+	@GetMapping({ "", "/" })
+	public String listAll(Model model) throws WebsiteException {
+		try {
+			List<CountryDto> countries = countryService.getCountries();
+			model.addAttribute("countries", countries);
+			return "countries";
+		} catch (NotFoundException e) {
+			throw WebsiteException.NotFound();
+		} catch (ProducerApiException e) {
+			throw WebsiteException.Internal(e);
+		}
+	}
 }
